a1b859d820653d6f2a0ba378add973938d50739a
unknown <marcoantoniocoluccibonfim@gmail.com>
2025-04-24 11:11:17 -0300

    Criação das classes e atributos

--- paginas/models.py
+++ paginas/models.py
@@ -1,3 +1,43 @@
 from django.db import models
 
-# Create your models here.
+# Todas as classes devem ter herança de models.Model
+
+# Cria suas classes
+
+# para objts - chaves extrangeira: (ForeignKey(referenciar qual é a classe, on_delete=models.PROTECT))
+
+class Country(models.Model):
+    # Definir atributos
+    name = models.CharField(max_length=100)
+    iso_code = models.CharField(max_length=2)
+
+
+    
+
+class EconomicBloc(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class CountryBlocMembership(models.Model):
+    country = models.ForeignKey(Country, on_delete=models.PROTECT)
+    bloc = models.ForeignKey(EconomicBloc, on_delete=models.PROTECT)  
+    start = models.DateField()  
+    end = models.DateField()  
+    
+
+class TradeData(models.Model):
+    country = models.ForeignKey(Country, on_delete=models.PROTECT)
+    year = models.PositiveSmallIntegerField(default=0)
+    export_value = models.FloatField()
+    import_value = models.FloatField()
+    trade_balance = models.FloatField()
+    gdp_share = models.FloatField()
+    world_trade_share = models.FloatField()
+
+
+class Kpi(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=100)
+    value = models.FloatField()
+    year = models.PositiveSmallIntegerField(default=0)
+    country = models.ForeignKey(Country, on_delete=models.PROTECT)
