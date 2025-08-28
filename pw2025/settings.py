4e9ad3ca764f1f204fb498927db2de28232beac2
unknown <marcoantoniocoluccibonfim@gmail.com>
2025-08-28 09:51:26 -0300

    Problema ao rodar runserver resolvido

--- pw2025/settings.py
+++ pw2025/settings.py
@@ -75,16 +75,23 @@ WSGI_APPLICATION = "pw2025.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#         "default": {
+#             "ENGINE": "django.db.backends.postgresql",
+#             "NAME": "postgres",
+#             "USER": "postgres.zcbomuoemhjexusztsov",
+#             "PASSWORD": "euamomulheres",
+#             "HOST": "aws-0-sa-east-1.pooler.supabase.com",
+#             "PORT": "5432",
+#         }
+#     }
 DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.postgresql",
-            "NAME": "postgres",
-            "USER": "postgres.zcbomuoemhjexusztsov",
-            "PASSWORD": "euamomulheres",
-            "HOST": "aws-0-sa-east-1.pooler.supabase.com",
-            "PORT": "5432",
-        }
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
+}
+
 
 
 
