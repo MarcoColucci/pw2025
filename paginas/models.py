a3c0f0406a6cd3825cf63c032767dd37d3fbf340
Marco Antonio <marcoantoniocoluccibonfim@gmail.com>
2025-09-07 20:40:12 -0300

    Classes de login e logout criadas.

--- paginas/models.py
+++ paginas/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Todas as classes devem ter herança de models.Model
 
 # Cria suas classes
@@ -7,10 +7,7 @@ from django.db import models
 # para objts - chaves extrangeira: (ForeignKey(referenciar qual é a classe, on_delete=models.PROTECT))
 
 class Country(models.Model):
-    # Definir atributos
-    # gpd = pib
-    # hdi = idh
-
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="countries")
     name = models.CharField(max_length=100)
     gpd = models.CharField(max_length=100)
     hdi = models.CharField(max_length=100)
@@ -21,28 +18,37 @@ class Country(models.Model):
 
 
 class EconomicBlock(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="economic_blocks")
     name = models.CharField(max_length=100)
 
 
 class BlockMembership(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="block_memberships")
     country = models.ForeignKey(Country, on_delete=models.PROTECT)
     block = models.ForeignKey(EconomicBlock, on_delete=models.PROTECT)  
     start_year= models.DateField()  
     end_year = models.DateField()  
-    
+
+
 class EconomicSector(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="economic_sectors")
     name = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
 
 
-class Economy(models.Model):
-    country = models.ForeignKey(Country, on_delete=models.PROTECT)
+# 🔹 Nova tabela para armazenar indicadores econômicos
+class EconomicIndicator(models.Model):
+    country = models.ForeignKey(Country, on_delete=models.CASCADE, related_name="ecomic_indicators")
     year = models.IntegerField()
-    export_value = models.FloatField()
-    import_value = models.FloatField()
-    sector = models.ForeignKey(EconomicSector, on_delete=models.PROTECT)
+
+    gdp = models.FloatField(help_text="PIB total em dólares")
+    gdp_per_capita = models.FloatField(help_text="PIB per capita em dólares")
+    population_total = models.BigIntegerField(help_text="População total")
+    imports_goods_services = models.FloatField(help_text="Importações de bens e serviços (% do PIB)")
+    exports_goods_services = models.FloatField(help_text="Exportações de bens e serviços (% do PIB)")
+    high_tech_exports = models.FloatField(help_text="Exportações de alta tecnologia (% das exportações manufatureiras)")
 
     def __str__(self):
-        return f"{self.country.name} - {self.year} - {self.sector.name}"
+        return f"{self.country.name} - {self.year}"
--- paginas/urls.py
+++ paginas/urls.py
@@ -4,7 +4,7 @@ from .views import Inicio, SobreView, MapView
 from .views import CountryCreate, EconomicBlockCreate, EconomicSectorCreate, BlockMembershipCreate, EconomyCreate
 from .views import CountryDelete, EconomicBlockDelete, EconomicSectorDelete, BlockMembershipDelete, EconomyDelete
 from .views import CountryUpdate, EconomicBlockUpdate, EconomicSectorUpdate, BlockMembershipUpdate, EconomyUpdate
-
+from django.contrib.auth import views as auth_views
 from .views import CountryList
 
 urlpatterns = [
@@ -16,6 +16,11 @@ urlpatterns = [
     path("register/economicsector", EconomicSectorCreate.as_view(), name="add-economicsector"),
     path("register/blockmembership/", BlockMembershipCreate.as_view(), name="add-blockmembership"),
     path("register/economy/", EconomyCreate.as_view(), name="add-economy"),
+
+    path('login/', auth_views.LoginView.as_view(template_name='paginas/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
+    path('password_change/', auth_views.PasswordChangeView.as_view(template_name='paginas/password_change.html'), name='password_change'),
+    path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(template_name='paginas/password_change_done.html'), name='password_change_done'),
 
 # fazer delete, e update
 
